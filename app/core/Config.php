ffa5ac681bf857de36d0514582433d57639f21b2
huukha04 <143921219+huukha04@users.noreply.github.com>
2025-05-13 14:24:13 +0700

    Update Config.php

--- app/core/Config.php
+++ app/core/Config.php
@@ -7,9 +7,9 @@ define('DBPASS', '');
 define('DBDRIVER', 'mysql');
 
 // Kết nối PayOS
-define('CLIENT_ID', 'a5cf32ce-c7fe-4ad0-8631-6a0104715c3a');
-define('API_KEY', '6bb17dfc-241d-448e-9478-21835d991d75');
-define('CHECKSUM_KEY', '5a52340ffa42156dfdf9ae3e32e1951f0058839e5702bdba92114f12a135aacd');
+define('CLIENT_ID', '');
+define('API_KEY', '');
+define('CHECKSUM_KEY', '');
 define('WAIT_TIME', 60 * 60 * 24 * 30); // 30 ngày
 
 
